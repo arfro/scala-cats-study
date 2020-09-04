643668bcba1777b70c544828605c51a07138172e
arfro <aerfromenes@gmail.com>
2020-09-04 11:23:02 +0200

    type class: implementing extension method

--- src/main/scala/com/catstudy/recap/TypeClassesRecap.scala
+++ src/main/scala/com/catstudy/recap/TypeClassesRecap.scala
@@ -13,14 +13,14 @@ object TypeClassesRecap {
   implicit object StringSerializer extends JsonSerializer[String] { // for String
     override def toJson(value: String): String =
       s"""
-         |"$value
+         |$value
          |""".stripMargin.trim
   }
 
   implicit object PersonSerializer extends JsonSerializer[Person] { // for Person
     override def toJson(value: Person): String =
       s"""
-         |"{"name": "${value.name}", "age": ${value.age}}
+         |{"name": "${value.name}", "age": ${value.age}}
          |""".stripMargin.trim
   }
 
@@ -31,6 +31,15 @@ object TypeClassesRecap {
     list.map(v => jsonSerializer.toJson(v)).mkString("[", ",", "]")
   }
 
+  // part 4. (optional!) EXTENSION METHODS
+  object JsonOps {
+    implicit class JsonSerializable[T](value: T)(
+      implicit jsonSerializer: JsonSerializer[T]
+    ) {
+      def toJson: String = jsonSerializer.toJson(value)
+    }
+  }
+
   // just some case classes that we can use with the code above
   case class Person(name: String, age: Int)
 
@@ -38,5 +47,7 @@ object TypeClassesRecap {
     // this can be run on any List of type class instances that we already have.
     // Right now we can only run it on String and Person (those two we have defined implicit object instances for)
     println(convertListToJson(List(Person("abc", 3), Person("def", 7))))
+    import JsonOps._ // this is where our Extension method is
+    println(Person("ggg", 4).toJson)
   }
 }
